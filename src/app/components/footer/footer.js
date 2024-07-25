64e7f8da41fc8d504f797afffad433b459088a98
courios-minion <okarskaolya@gmail.com>
2024-07-25 12:11:54 +0200

    added adaptive footer

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -2,8 +2,12 @@ import Image from "next/image";
 import {
 	footerContainer,
 	footerContentContainer,
+	adaptiveFooterHeader,
+	footerHeaderHidden,
+	footerHeader,
 	icons,
-	contacts,
+	footerContacts,
+	contacts
 } from "./footer.module.css";
 
 import mapPoint from "../../../../public/svg/mapPoint.svg";
@@ -11,29 +15,43 @@ import phone from "../../../../public/svg/phone.svg";
 import mail from "../../../../public/svg/mail.svg";
 
 export default function Footer() {
+
+	const currentYear = new Date().getFullYear();
 	return (
 		<footer className={footerContainer}>
-				<div className={footerContentContainer}>
-					<h5>
-					©1900-2024 Національний університет сингулярних технологій
+			<div className={footerContentContainer}>
+				<div className={adaptiveFooterHeader}>
+						<h5 className={footerHeaderHidden}>
+					©1900-{currentYear} <br/>Національний університет сингулярних технологій
 
 				</h5>
-				<div className={contacts}>
-					<Image className={ icons} src={mapPoint} alt=""/>
-
-					<p>
-						вул. Академічна, 12, м. Київ, Україна
-					</p>
-					<Image className={ icons} src={phone} alt=""/>
-
-					<p>
-						 +38 (044) 123-45-67
-          </p>
-		  	<Image className={ icons} src={mail} alt=""/>
-					<p>
-						info@university.edu
-          </p>
 				</div>
+
+					<h5 className={footerHeader}>
+					©  1900-{currentYear} Національний університет сингулярних технологій
+
+				</h5>
+				<ul className={contacts}>
+
+					<li className={footerContacts}>
+						<Image className={ icons} src={mapPoint} alt=""/>
+						<p>02000 Україна, м. Київ, вул. Хрещатик, 1 </p>
+					</li>
+
+					<li className={footerContacts}>
+						<Image className={ icons} src={phone} alt=""/>
+						<p > +38 (044) 123-45-67</p>
+					</li>
+
+					<li className={footerContacts}>
+						<Image className={ icons} src={mail} alt=""/>
+						<p >info@university.edu
+					</p>
+					</li>
+
+
+
+				</ul>
 				</div>
 
 			</footer>
