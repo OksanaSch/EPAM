7cf40cb2663857e688091f7fe89bf62a18805ec6
courios-minion <okarskaolya@gmail.com>
2024-07-27 13:52:06 +0200

    edited contacts page

--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -9,11 +9,12 @@ import {
 	phoneTitle,
 	mailTitle,
 	mailContainer,
-	formContainer,
+	contactsForm,
 	formLables,
 	nameBox,
 	mailBox,
-	massageBox,
+	messageBox,
+	mailContainerButton,
 	forms,
 	map,
 	mainSection
@@ -30,7 +31,6 @@ export default function Contacts () {
 		event.preventDefault();
 		event.stopPropagation();
 	  }
-  
 	  setValidated(true);
 	};
 
@@ -38,25 +38,30 @@ export default function Contacts () {
 
 
 	return (
-	
 			<main className={contactsContainer}>
-				<section className={mainSection}>
-				
+			<section className={mainSection}>
+
 					<section className={contacts}>
 						<h1>Контакти</h1>
 						<p className={adressTitle}>Адреса:</p>
 						<p>Національний університет сингулярних технологій,<br></br> 02000, Україна, м. Київ, вул. Хрещатик, 1</p>
-						
+
 						<p className={phoneTitle}>Телефон:</p>
 						<p>+38 (044) 123-45-67</p>
 						<p className={mailTitle}>Електронна пошта:</p>
 						<p>info@nut.edu.ua</p>
-					</section>
+				</section>
+					<div>
+					<section className={map}>
+
+		 		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.4486373518894!2d30.524924076557287!3d50.45136987159231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce51c3bdb9a1%3A0xff877737cf946b28!2z0LLRg9C70LjRhtGPINCl0YDQtdGJ0LDRgtC40LosIDEsINCa0LjRl9CyLCAwMjAwMA!5e0!3m2!1suk!2sua!4v1721109039187!5m2!1suk!2sua" width="970" height="962"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+				</section>
 					<section className={mailContainer}>
-						
-						<h2>Написати нам</h2>					
-						<Form noValidate validated={validated} onSubmit={handleSubmit}> 
-						<div className={formContainer}>
+
+						<h2>Написати нам</h2>
+					<Form noValidate validated={validated} onSubmit={handleSubmit} className={contactsForm}>
+						{/* <div className={formContainer}> */}
 						<div className={forms}>
 							<Form.Label className={formLables}>Ім'я</Form.Label>
 							<Form.Control  className={nameBox}  required type="text"/>
@@ -66,25 +71,17 @@ export default function Contacts () {
 							<Form.Control className={mailBox}  required type="email"/>
 							</div>
 							<div className={forms}>
-							<Form.Label className={formLables}>Повідомлення</Form.Label>		
-							<Form.Control className={massageBox}  required as="textarea" rows={3} ></Form.Control>
+							<Form.Label className={formLables}>Повідомлення</Form.Label>
+							<Form.Control className={messageBox}  required as="textarea" rows={3} ></Form.Control>
 							</div>
-							</div>
-							<button type="submit">Надіслати</button>
+							{/* </div> */}
+							<button type="submit" className={mailContainerButton}>Надіслати</button>
 					</Form>
-					
-		
+
+
 				</section>
-				
-					
-				</section>
-				<section className={map}>
-				
-		 		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.4486373518894!2d30.524924076557287!3d50.45136987159231!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce51c3bdb9a1%3A0xff877737cf946b28!2z0LLRg9C70LjRhtGPINCl0YDQtdGJ0LDRgtC40LosIDEsINCa0LjRl9CyLCAwMjAwMA!5e0!3m2!1suk!2sua!4v1721109039187!5m2!1suk!2sua" width="970" height="962"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-		 	
+				</div>
 				</section>
 			</main>
-		
-		//  
 	)
 }
